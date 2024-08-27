24a86fd485f7a8a5db6510b05b4eb5ea689a6ca8
Jose Henrique <jose15000henrique@outlook.com>
2024-08-27 20:10:58 -0300

    Ajustes

--- main.py
+++ main.py
@@ -12,29 +12,34 @@ class LexicalError(Exception):
 
     def _format_message(self):
         return f"Erro léxico na linha {self.line}, coluna {self.column}: '{self.text}'"
-    
 
 class CustomLexer(gramaticaLexer):
     def notifyListeners(self, e):
-        msg = self._formatMessage(self.line, self.column, self.text)
-        raise LexicalError(self.line, self.column, self.text)
-
-    def _formatMessage(self, line, column, text):
-        return f"Erro léxico na linha {line}, coluna {column}: '{text}'"
+        line = self._tokenStartLine
+        column = self._tokenStartColumn
+        text = self._input.getText(self._tokenStartCharIndex, self._input.index)
+        raise LexicalError(line, column, text)
 
 def main():
     try:
         text = input("> ")
 
         lexer = CustomLexer(InputStream(text))
-
         stream = CommonTokenStream(lexer)
-
         parser = gramaticaParser(stream)
 
         tree = parser.programa()
-
         print(tree.toStringTree(recog=parser))
+
+        for token in stream.tokens:
+            token_text = token.text
+            token_type = lexer.symbolicNames[token.type]
+            
+            if ':' in token_text:
+                tipo, atributo = token_text.split(":")
+                print(f"Token: {token_text}, Tipo: {tipo}, Atributo: {atributo}")
+            else:
+                print(f"Token: {token_text}, Tipo: {token_type}")
 
         print("Análise léxica concluída.")
 
